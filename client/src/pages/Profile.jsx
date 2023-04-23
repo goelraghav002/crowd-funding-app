bd07568d92843f7f3ce2faf8032228234b161f17
Raghav Goel <goelraghav002@gmail.com>
2023-04-23 20:46:12 +0530

    this is the final commit

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,64 +1,92 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import { DisplayCampaigns } from '../components';
-import { useStateContext } from '../context'
+import { useStateContext } from '../context';
 import { Navigate } from 'react-router-dom';
 import { userInfo } from '../helpers/apiRequest';
 
 const Profile = () => {
-  
-  const token = window.localStorage.getItem('token');
-  const [isLoading, setIsLoading] = useState(false);
-  const [campaigns, setCampaigns] = useState([]);
+	const token = window.localStorage.getItem('token');
+	const [isLoading, setIsLoading] = useState(false);
+	const [campaigns, setCampaigns] = useState([]);
 
-  const { address, contract, getUserCampaigns } = useStateContext();
+	const { address, contract, getUserCampaigns } = useStateContext();
 
-  const [user, setUser] = useState(null);
+	const [user, setUser] = useState(null);
 
 	useEffect(() => {
 		if (token) {
 			userInfo().then((data) => setUser(data.user));
 		}
-  }, []);
-  
-  console.log(user);
+	}, []);
 
-  const fetchCampaigns = async () => {
-    setIsLoading(true);
-    const data = await getUserCampaigns();
-    setCampaigns(data);
-    setIsLoading(false);
-  }
+	console.log(user);
 
-  if (!token){
-    return <Navigate to="/login" replace />
-  }
+	const fetchCampaigns = async () => {
+		setIsLoading(true);
+		const data = await getUserCampaigns();
+		setCampaigns(data);
+		setIsLoading(false);
+	};
 
-  useEffect(() => {
-    if(contract) fetchCampaigns();
-  }, [address, contract]);
+	if (!token) {
+		return <Navigate to='/login' replace />;
+	}
 
-  return (
-    <>
-      <div className='text-white mb-10'>
-        <h4 className='font-bold text-[30px] my-2'>User Details</h4>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Name: </span>{user?.name}</p>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Description: </span>{user?.description}</p>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Gov Id Type: </span>{user?.govIdType}</p>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Gov Id Number: </span>{user?.govIdNumber}</p>
-        <h4 className='font-bold text-[20px] my-4'>Contact Details</h4>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Contact Number: </span>{user?.contact1}</p>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Altername Number: </span>{user?.contact2}</p>
-        <p className='mt-3 text-white'>&nbsp;&nbsp;<span className='font-bold text-gray-400'>Email: </span>{user?.email}</p>
+	useEffect(() => {
+		if (contract) fetchCampaigns();
+	}, [address, contract]);
 
-      </div>
-      <DisplayCampaigns 
-        title="User Campaigns"
-        isLoading={isLoading}
-        campaigns={campaigns}
-      />
-    </>
-  )
-}
+	return (
+		<>
+			<div className='text-white mb-10 flex items-start'>
+				<div className='mr-40'>
+					<h4 className='font-bold text-[20px] my-2'>User Details</h4>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;<span className='font-bold text-gray-400'>Name: </span>
+						{user?.name}
+					</p>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;
+						<span className='font-bold text-gray-400'>Description: </span>
+						{user?.description}
+					</p>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;
+						<span className='font-bold text-gray-400'>Gov Id Type: </span>
+						{user?.govIdType}
+					</p>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;
+						<span className='font-bold text-gray-400'>Gov Id Number: </span>
+						{user?.govIdNumber}
+					</p>
+				</div>
+				<div>
+					<h4 className='font-bold text-[20px] my-4'>Contact Details</h4>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;
+						<span className='font-bold text-gray-400'>Contact Number: </span>
+						{user?.contact1}
+					</p>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;
+						<span className='font-bold text-gray-400'>Altername Number: </span>
+						{user?.contact2}
+					</p>
+					<p className='mt-3 text-white'>
+						&nbsp;&nbsp;<span className='font-bold text-gray-400'>Email: </span>
+						{user?.email}
+					</p>
+				</div>
+			</div>
+			<DisplayCampaigns
+				title='User Campaigns'
+				isLoading={isLoading}
+				campaigns={campaigns}
+			/>
+		</>
+	);
+};
 
-export default Profile
+export default Profile;
